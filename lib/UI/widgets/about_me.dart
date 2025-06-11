39d033b464acef534a85b63bd535e0caff824cf4
ErikVieiraFer <erik.vieiradev@hotmail.com>
2025-06-11 13:32:20 -0300

    favicon alterado

--- lib/UI/widgets/about_me.dart
+++ lib/UI/widgets/about_me.dart
@@ -21,7 +21,7 @@ class AboutMe extends StatelessWidget {
           Text('Sobre Mim', style: Theme.of(context).textTheme.displayLarge),
           const SizedBox(height: 12),
           Text(
-            'Estou começando minha carreira como desenvolvedor, com foco na criação de aplicativos com Flutter e no desenvolvimento web utilizando HTML e CSS. Tenho experiência inicial com C# e estou constantemente buscando aprimorar minhas habilidades por meio da construção de novos projetos e da prática contínua. Meu objetivo é ganhar experiência no mercado e evoluir profissionalmente.',
+            'Estou começando minha carreira como desenvolvedor, com foco na criação de aplicativos com Flutter e no desenvolvimento web utilizando HTML, CSS e JavaScript. Tenho experiência inicial com C# e estou constantemente buscando aprimorar minhas habilidades por meio da construção de novos projetos e da prática contínua. Meu objetivo é ganhar experiência no mercado e evoluir profissionalmente.',
             style: GoogleFonts.orbitron(fontSize: 16, color: Colors.white70),
           ),
         ],
