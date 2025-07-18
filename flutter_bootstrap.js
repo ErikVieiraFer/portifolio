4cd57aae5a91deb51f8068523e16f67cdb8640c7
ErikVieiraFer <erik.vieiradev@hotmail.com>
2025-07-18 16:30:08 -0300

    Deploy final e correto

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4007502130"
+    serviceWorkerVersion: "2648292673"
   }
 });
