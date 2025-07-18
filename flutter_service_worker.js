023ad895298b1acf15c0bed09248364ecaa4aae1
ErikVieiraFer <erik.vieiradev@hotmail.com>
2025-07-18 17:08:47 -0300

    Deploy com correção do caminho base

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "33483169777212845466d53151a0aea8
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "df95228b41521237b92eef25c5af55c3",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "09dcec5587c0295fed29de095f1bedf3",
+"flutter_bootstrap.js": "edb0287b0c1d3a1932a8e4779460385a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
